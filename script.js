8a1dff41c7a3fa2c6c02616d2e953f87a0c5b673
SandroIerna <iernasandro@gmail.com>
2022-11-15 16:25:23 +0100

    adds basic fetch

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 console.log("hello");
+
+const options = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "0338074fd7mshc55ec8ce8050d51p1f1e97jsn507294c645ff",
+    "X-RapidAPI-Host": "deezerdevs-deezer.p.rapidapi.com",
+  },
+};
+
+getSomething = async () => {
+  let response = await fetch(
+    "https://striveschool-api.herokuapp.com/api/deezer/search?q=queen",
+    options
+  );
+  let data = await response.json();
+  console.log(data.data[0]);
+};
