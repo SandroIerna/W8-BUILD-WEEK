f6f044bf4578b318bf16eabb7a98a313e5522605
codewithjhey <114162135+codewithjhey@users.noreply.github.com>
2022-11-16 14:52:57 +0000

    album js push

--- album.js
+++ album.js
@@ -19,32 +19,58 @@ getAlbum = async (id) => {
   console.log(data.cover)
   console.log(data.cover_small)
   console.log(data.artist.name)
+  let length = data.tracks.data.length
 
-  let trackList = document.getElementById("trackInfo")
+  let albumInfo = document.getElementById("trackInfo")
 
-  trackList.innerHtml = `
-//   <div class="card ml-4 mt-5" style="width: 18rem">
-//       <img
-//         src="${data.cover}"
-//         class="card-img-top"
-//         alt="album-image"
-//       />
-//     </div>
-//     <div  class="d-flex w-100 align-items-end ml-4">
-//       <div>
-//         <h3>ALBUM</h3>
-//         <h1>"{data.title}"</h1>
-//         <h3 class="artist-info-banner">
-//           <img
-//             class="avatar"
-//             src="${data.cover_small}"
-//             alt=""
-//           />
-//           <strong>${data.artist.name}" </strong> - 2022 - 13 songs, 40 min 53 sec
-//         </h3>
-//       </div>
-//     </div>
+  albumInfo.innerHTML = `
+  <div class="card ml-4 mt-5" style="width: 18rem">
+      <img
+        src="${data.cover}"
+        class="card-img-top"
+        alt="album-image"
+      />
+    </div>
+    <div  class="d-flex w-100 align-items-end ml-4">
+      <div>
+        <h3>ALBUM</h3>
+        <h1>${data.title}</h1>
+        <h3 class="artist-info-banner">
+          <img
+            class="avatar"
+            src="${data.cover_small}"
+            alt=""
+          />
+          <strong>${data.artist.name}</strong> - ${data.release_date} - ${length} - ${data.duration}
+        </h3>
+      </div>
+    </div>
     `
-}
 
-getAlbum(370666207)
+  let trackList = document.getElementById("trackList")
+
+  let trackArray = data.tracks.data
+  console.log(data.tracks.data)
+  trackArray.forEach((track, index) => {
+    trackList.innerHTML += `
+    <ul id="album-list">
+   <li>
+   <div class="d-flex justify-content-between">
+   <div class="d-flex align-items-center">
+   <div class="mr-5">
+     <p class="info-list-paragraph">${index + 1}</p>
+    </div>
+       <div>
+     <p class="info-list-paragraph">
+       <strong>${track.title}</strong>
+     </p>
+     <p class="info-list-paragraph">${data.artist.name}</p>
+   </div>
+   </div>
+   <div class="mr-4">
+   <p class="info-list-paragraph">${track.duration} seconds</p>
+    </div>
+   </div>
+    </li>`
+  })
+}
