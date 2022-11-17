1bdb9ba7b03353ef82326bccb16a83cb55e67302
SandroIerna <iernasandro@gmail.com>
2022-11-17 16:32:17 +0100

    final mp3 player

--- player.js
+++ player.js
@@ -1,19 +1,4 @@
-// const audio = new Audio(
-//   "https://cdns-preview-2.dzcdn.net/stream/c-222c7d6335ba3cfa69585e781bb51946-8.mp3"
-// );
-// // audio.id = "audio";
-// console.log(audio);
-// function play() {
-//   audio.play();
-// }
-// function pause() {
-//   audio.pause();
-// }
-
-// const audio = new Audio(
-//   "https://cdns-preview-2.dzcdn.net/stream/c-222c7d6335ba3cfa69585e781bb51946-8.mp3"
-// );
-handlePlaySong = async (audio) => {
+const handlePlaySong = async (audio) => {
   console.log(audio);
   if (audio.paused) {
     audio.play();
@@ -21,61 +6,18 @@ handlePlaySong = async (audio) => {
     audio.pause();
   }
 };
-let audio = "";
-playerDisplayAlbum = async (artist) => {
+
+const handleSong = async (albumID, trackID) => {
   const response = await fetch(
-    `https://striveschool-api.herokuapp.com/api/deezer/search?q=${artist}`
+    `https://striveschool-api.herokuapp.com/api/deezer/album/${albumID}`
   );
   const data = await response.json();
-  //   console.log(data);
-  const albumArray = data.data;
-  //   console.log(albumArray[0]);
-  const song = albumArray[0].preview;
+  let tracksArray = data.tracks.data;
+  console.log(tracksArray[0].id);
+  let filteredArray = tracksArray.filter((track) => track.id === trackID);
+  console.log(tracksArray);
+  console.log(filteredArray);
+  song = filteredArray[0].preview;
   console.log(song);
   audio = new Audio(`${song}`);
 };
-
-// const playerDisplayAlbum = (song) => {
-//   async (artist) => {
-//     const response = await fetch(
-//       `https://striveschool-api.herokuapp.com/api/deezer/search?q=${artist}`
-//     );
-//     const data = await response.json();
-
-//     const albumArray = data.data;
-//     console.log(albumArray[0]);
-//     const song = albumArray[0].preview;
-//     console.log(song);
-//     const audio = new Audio(`${song}`);
-//     audio.id = "audio";
-//     console.log(audio);
-//     function play() {
-//       audio.play();
-//     }
-//     function pause() {
-//       audio.pause();
-//     }
-
-//     const playbtn = document.querySelector("#play-btn");
-//     const playbtnPlay = document.querySelector("#button-element-play");
-//     const playbtnPause = document.querySelector("#button-element-pause");
-//     playbtnPlay.addEventListener("click", () => {
-//       playbtn.innerHTML = `(
-//       <button id="button-element-pause">
-//         <img src="./icons/pause.svg" alt="" />
-//       </button>
-//     )`;
-//     });
-//     playbtnPause.addEventListener("click", () => {
-//       playbtn.innerHTML = `(
-//       <button id="button-element-play">
-//         <img src="./icons/play.svg" alt="" />
-//       </button>
-//     )`;
-//     });
-//   };
-// };
-
-// playerDisplayAlbum(
-//   "https://cdns-preview-2.dzcdn.net/stream/c-222c7d6335ba3cfa69585e781bb51946-8.mp3"
-// );
